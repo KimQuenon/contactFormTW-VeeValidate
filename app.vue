ba148b510c8def1355b785bb43fa18ac5fc674d5
Kim <kimberley.quenon.@gmail.com>
2024-02-28 11:19:53 +0100

    tailwind install + bases

--- app.vue
+++ app.vue
@@ -1,37 +1,8 @@
-
 <template>
-  <div>
-    <Form @submit="onSubmit" :validation-schema="schema">
-      <Field name="email" type="email" class="form-control" :rules="validateEmail"/>
-      <ErrorMessage name="email" />
-
-      <button type="submit" class="btn btn-primary">Sign up for newsletter</button>
-    </Form>
+  <div class="flex items-center h-screen">
+    <div class="container container-home mx-auto max-w-lg p-8 flex items-center justify-center flex-col rounded-xl shadow-2xl">
+      <h1 class="font-bold t-4 mb-4 text-3xl">Contactez-nous</h1>
+      <Form></Form>
+    </div>
   </div>
 </template>
-
-<script setup>
-  import { Form, Field, ErrorMessage  } from 'vee-validate';
-
-   const onSubmit = (values) => {
-    console.log(values)
-   }
-
-   const validateEmail = (value)=> {
-      // if the field is empty
-      if (!value) {
-        return 'This field is required';
-      }
-
-      // if the field is not a valid email
-      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-      if (!regex.test(value)) {
-        return 'This field must be a valid email';
-      }
-
-      // All is good
-      return true;
-    }
-
-</script>
-
