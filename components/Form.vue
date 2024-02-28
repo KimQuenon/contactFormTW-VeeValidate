a69ae67e6f968b20dd5cca457dd90f94b31f7649
Kim <kimberley.quenon.@gmail.com>
2024-02-28 11:35:36 +0100

    form bases

--- components/Form.vue
+++ components/Form.vue
@@ -1,56 +1,90 @@
+<script setup>
+    import { Form, Field, ErrorMessage  } from 'vee-validate';
+
+</script>
+
+
 <template>
-    <form @submit.prevent="handleSubmit" :validation-schema="schema">
-      <div>
-        <label>Nom</label>  
-        <input
-          type="text"
-          v-model="name"
-          class="form-control"
-          placeholder="Votre nom...*"
-          required="required"
-        >
-      </div>
-      <div>
-        <label>Prénom</label>  
-        <input
-          type="text"
-          v-model="surname"
-          class="form-control"
-          placeholder="Votre prénom...*"
-          required="required"
-        >
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type="email"  
-          v-model="email"
-          class="form-control"
-          placeholder="Votre adresse mail...*"
-          required="required"
-        >
-      </div>
-      <div>
-        <label>Téléphone</label>
-        <input
-          type="tel"  
-          v-model="email"
-          class="form-control"
-          placeholder="Votre numéro de téléphone..."
-        >
-      </div>
-      <div>
-        <label>Message</label>
-        <textarea
-          type="text"
-          v-model="message"
-          class="form-control"
-          placeholder="Message"
-          required="required"
-        ></textarea>
-      </div>
-      <div>
-        <button type="submit" class="btn">Envoyer</button>
-      </div>
-    </form>
-</template>
+    <div>
+      <Form @submit="onSubmit" :validation-schema="schema">
+
+        <div class="mb-4">
+          <label>Nom<span class="text-red-600">*</span></label>
+          <div class="relative">
+            <Field
+              type="text"
+              name="name"
+              class="form-input block w-full border border-gray-300 rounded px-3 py-2"
+              placeholder="Votre nom..."
+              :rules="validateName"
+            >
+            </Field>
+            <ErrorMessage name="name" class="text-xs text-red-500"/>
+          </div>
+        </div>
+
+        <div class="mb-4">
+          <label>Prénom<span class="text-red-600">*</span></label>
+          <div class="relative">
+            <Field
+              type="text"
+              name="surname"
+              class="form-input block w-full border border-gray-300 rounded px-3 py-2"
+              placeholder="Votre prénom..."
+              :rules="validateSurname"
+            >
+            </Field>
+            <ErrorMessage name="surname" class="text-xs text-red-500"/>
+          </div>
+        </div>
+
+        <div class="mb-4">
+          <label>Mail<span class="text-red-600">*</span></label>
+          <div class="relative">
+            <Field
+              type="email"
+              name="mail"
+              class="form-input block w-full border border-gray-300 rounded px-3 py-2"
+              placeholder="Votre adresse mail..."
+              :rules="validateEmail"
+            >
+            </Field>
+            <ErrorMessage name="mail" class="text-xs text-red-500"/>
+          </div>
+        </div>
+
+        <div class="mb-4">
+          <label>Phone<span class="text-red-600">*</span></label>
+          <div class="relative">
+            <Field
+              type="tel"
+              name="phone"
+              class="form-input block w-full border border-gray-300 rounded px-3 py-2"
+              placeholder="Votre numéro de téléphone..."
+              :rules="validatePhone"
+            >
+            </Field>
+            <ErrorMessage name="phone" class="text-xs text-red-500"/>
+          </div>
+        </div>
+
+        <div class="mb-4">
+          <label>Message<span class="text-red-600">*</span></label>
+          <div class="relative">
+            <Field
+              type="text"
+              name="message"
+              class="form-input block w-full border border-gray-300 rounded px-3 py-2"
+              placeholder="Commencez à rédiger..."
+              :rules="validatePhone"
+              as="textarea"
+            >
+            </Field>
+            <ErrorMessage name="message" class="text-xs text-red-500"/>
+          </div>
+        </div>
+  
+        <button type="submit" class="btn btn-primary">Envoyer</button>
+      </Form>
+    </div>
+  </template>
